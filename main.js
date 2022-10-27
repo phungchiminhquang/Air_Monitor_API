678aafd4c64d91838cd3f8d57cf8336e0c816229
MinhQuang <phungchiminhquang@gmail.com>
2022-10-27 19:43:11 +0700

    change PORT to suitable to environment where it's deploied

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/station", stationRouter);
 
-app.listen(3000, () => {
-  console.log("listening on PORT 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("listening on PORT " + PORT);
 });
